7b60ada68f0be8aabf845a3ee10f3b53fceeb468
a-llie <alexstefanakis@cmail.carleton.ca>
2023-10-28 15:19:19 -0400

    preliminary code

---
+++ imageCompare.js
@@ -0,0 +1,100 @@
+const Jimp = require('jimp');
+const fs = require('fs');
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+
+let albumns = {};
+const r1 = readline.createInterface({ input, output });
+inputLoop();
+
+function CreateNewAlbum(albumnName)
+{
+  if (albumns[albumnName] == null) { 
+    albumns[albumnName] = {}; 
+    albumns[albumnName].processedImages = new Set();
+    albumns[albumnName].images = [];
+    console.log("Albumn created");
+  }
+  else { console.log("[ERR] Albumn already exists"); }
+}
+
+async function AddImagesToAlbum(albumnName, imageList)
+{
+    var files = fs.readdirSync(imageList);
+    await processImages(albumns[albumnName], files).then(() => { console.log(albumns)});
+}
+
+async function processImages(albumn,folder) {
+  for (let i = 0; i < folder.length; i++) {
+    if (albumn.processedImages.has('./img/' + folder[i])) { console.log("already processed" + './img/' + folder[i]); continue; }
+    await Jimp.read('./img/' + folder[i])
+    .then((result) => {
+      result.pathOrigin = './img/' + folder[i];
+      result.duplicates = new Set();
+      result.isDuplicate = false;
+      albumn.processedImages.add(result.pathOrigin);
+      albumn.images.push(result
+      .resize(512, 512))
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  }
+} 
+
+function comparePhotosArray(images) {
+  
+for (var i = 0; i < images.length - 1; i++) {
+   for (var j = i+1; j < images.length; j++) {
+     if (images[i].isDuplicate || images[j].isDuplicate) continue;
+     comparePhotos(images[i], images[j]);
+   }
+ }
+}
+
+async function comparePhotos(img1, img2)
+{
+  var distance = Jimp.distance(img1, img2); // perceived distance
+  var diff = Jimp.diff(img1, img2); // pixel difference
+  if ((distance < 0.15 && diff.percent < 0.4) || diff.percent < 0.05) {
+    img1.duplicates.add(img2.pathOrigin);
+    img2.isDuplicate = true;
+  }
+}
+
+async function inputLoop(){
+  r1.question("\nUse: \n  'create'  -> create new album \n  'add'     -> add images to album\n  'compare' -> find duplicates in album\n\n>> ", answer => { 
+    if (answer == 'create'){
+      r1.question("Albumn Name: ", albumnName => {
+        CreateNewAlbum(albumnName);
+        inputLoop();
+      });
+    }
+    else if (answer == 'add'){
+        r1.question("Albumn Name: >> ", albumnName => {
+            r1.question("Images folder: >> ", images => {
+              AddImagesToAlbum(albumnName, images).then(() => { inputLoop(); });
+              
+            });
+        });
+      }
+      else if (answer == 'compare'){
+        r1.question("Albumn Name: >> ", albumnName => {
+          comparePhotosArray(albumns[albumnName].images);
+          console.log("Duplicate groups: ");
+          
+          for (let i = 0; i < albumns[albumnName].images.length; i++) {
+            if (albumns[albumnName].images[i].duplicates.size == 0) continue;
+            console.log("\n");
+            console.log(albumns[albumnName].images[i].pathOrigin);
+            for (const el of albumns[albumnName].images[i].duplicates) console.log(el);
+          }
+          inputLoop();
+        });
+      }
+    else inputLoop();
+    });
+}
+
+
---
+++ imageCompare.py
@@ -0,0 +1,162 @@
+import cv2
+import face_recognition
+from PIL import Image
+import glob
+import time
+
+
+class Encoding: 
+    def __init__(self, originalPath, originalImage, img, encoding) -> None:
+        self.originalPath = originalPath
+        self.originalImage = originalImage
+        self.img = img
+        self.encoding = encoding
+        self.matches = set()
+
+    @property 
+    def get_img(self):
+        return self.img
+    
+    @property
+    def get_original_img(self):
+        return self.originalPath
+
+    @property
+    def get_matches(self):
+        return self.matches
+    
+    @property
+    def get_encoding(self):
+        return self.encoding
+    
+
+def find_face_encodings(image_path):
+    # reading image
+    image = cv2.imread(image_path)
+    # get face encodings from the image
+    face_enc = face_recognition.face_encodings(image)
+    # return face encodings
+    return face_enc[0]
+
+"""
+compares two face encodings 
+in: img1: face encoding
+    img2: face encoding 
+
+returns an image path if true, None if false 
+"""
+def compare_faces(img1, img2):
+    print(f"Comparing {img1.get_img} and {img2.get_img}")
+    is_same = face_recognition.compare_faces([img1.get_encoding], img2.get_encoding)[0]
+    if is_same:
+        # finding the distance level between images
+        return img2.get_original_img
+        distance = face_recognition.face_distance([img1.get_encoding], img2.get_encoding)
+        distance = round(distance[0] * 100)
+
+        img1.matches.add(img2)
+        img2.matches.add(img1)
+            
+        # calcuating accuracy level between images
+        accuracy = 100 - round(distance)
+        print(f"Face match:  {img2.get_original_img} & {img1.get_original_img} - Accuracy level: {accuracy}")
+    else:
+        return None
+
+"""
+finds all faces in a single image 
+in:  imagePath
+
+returns an array of face encodings 
+"""
+def find_all_faces_in_image(imagePath):
+    faces = []
+    img = face_recognition.load_image_file(imagePath)
+    face_locations = face_recognition.face_locations(img)
+    i = 0
+    for face_location in face_locations:
+        # Print the location of each face in this image
+        top, right, bottom, left = face_location
+        # You can access the actual face itself like this:
+        face_image = img[top:bottom, left:right]
+        new_image = Image.fromarray(face_image)
+        new_path = f"./saved_imgs/ref_img_{i}.jpg"
+        i+= 1
+        new_image.save(new_path)
+        try:
+            encoding = find_face_encodings(new_path) 
+        except:
+            continue
+        face = Encoding(originalPath=imagePath, originalImage=img, img=new_path, encoding=encoding)
+        faces.append(face)
+
+    return faces
+
+
+"""
+finds all photos containing a specific person 
+in:  reference Encoding type image to compare all others to 
+     array of Encodings being compared to reference image
+
+returns a set of image paths 
+"""
+def find_all_matches(refImg, faces): #finds all photos containing a specific person 
+    matches = set()
+    for img in faces:
+        if img.get_original_img in matches: 
+            continue 
+        result = compare_faces(refImg, img)    
+        if result is not None:
+            matches.add(result)        
+
+    return matches 
+
+
+"""
+finds all face encodings in a folder 
+in:  path to find all face encodings in 
+
+returns an array of face encodings  
+"""
+def find_faces_in_folder(path):
+    image_paths = glob.glob(path + "/*")
+
+    faces = []
+    i = 0
+    for image in image_paths:
+        img = face_recognition.load_image_file(image)
+        face_locations = face_recognition.face_locations(img)
+
+        for face_location in face_locations:
+            # Print the location of each face in this image
+            top, right, bottom, left = face_location
+            # You can access the actual face itself like this:
+            face_image = img[top:bottom, left:right]
+            pil_image = Image.fromarray(face_image)
+            new_path = f"./saved_imgs/img_{i}.jpg"
+            pil_image.save(new_path)
+            try:
+                encoding = find_face_encodings(new_path) 
+            except:
+                continue
+            i+= 1
+            face = Encoding(originalPath=image, originalImage=img, img=new_path, encoding=encoding)
+            faces.append(face)
+
+    return faces
+
+def main():
+    st = time.time()
+    path = './img/20230903_135847190_iOS.jpg'
+
+    ref_faces = find_all_faces_in_image(path)
+
+    ref_face = ref_faces[0]
+    compare_faces = find_faces_in_folder('./img')
+
+    result = find_all_matches(ref_face, compare_faces)
+
+    print(result)
+
+    
+main()
