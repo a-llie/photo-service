7795f131a6050ebf12789a77177f6889c6d21c6e
a-llie <alexstefanakis@cmail.carleton.ca>
2023-10-29 16:46:15 -0400

    updated python script to prompt user for image

--- imageCompare.py
+++ imageCompare.py
@@ -3,7 +3,9 @@ import face_recognition
 from PIL import Image
 import glob
 import time
-
+from pathlib import Path
+import sys
+import os
 
 class Encoding: 
     def __init__(self, originalPath, originalImage, img, encoding) -> None:
@@ -46,11 +48,11 @@ in: img1: face encoding
 returns an image path if true, None if false 
 """
 def compare_faces(img1, img2):
-    print(f"Comparing {img1.get_img} and {img2.get_img}")
     is_same = face_recognition.compare_faces([img1.get_encoding], img2.get_encoding)[0]
     if is_same:
         # finding the distance level between images
         return img2.get_original_img
+        #use below if  more info is wanted 
         distance = face_recognition.face_distance([img1.get_encoding], img2.get_encoding)
         distance = round(distance[0] * 100)
 
@@ -67,10 +69,10 @@ def compare_faces(img1, img2):
 finds all faces in a single image 
 in:  imagePath
 
-returns an array of face encodings 
+returns a dict of {path_to_cropped_face : cropped_image_source_path}
 """
 def find_all_faces_in_image(imagePath):
-    faces = []
+    face_paths = dict()
     img = face_recognition.load_image_file(imagePath)
     face_locations = face_recognition.face_locations(img)
     i = 0
@@ -83,14 +85,24 @@ def find_all_faces_in_image(imagePath):
         new_path = f"./saved_imgs/ref_img_{i}.jpg"
         i+= 1
         new_image.save(new_path)
-        try:
-            encoding = find_face_encodings(new_path) 
-        except:
-            continue
-        face = Encoding(originalPath=imagePath, originalImage=img, img=new_path, encoding=encoding)
-        faces.append(face)
+        face_paths.update({new_path :  imagePath})
 
-    return faces
+    return face_paths
+
+
+"""
+create a face encoding from a cropped image  
+in:  cropped_img_path, original_image_path
+
+returns a face encoding 
+"""
+def create_face_encoding(cropped_img_path, original_image_path):
+    try:
+        encoding = find_face_encodings(cropped_img_path) 
+        face = Encoding(originalPath=original_image_path, originalImage=face_recognition.load_image_file(original_image_path), img=cropped_img_path, encoding=encoding)
+        return face
+    except:
+        return None
 
 
 """
@@ -135,27 +147,79 @@ def find_faces_in_folder(path):
             pil_image = Image.fromarray(face_image)
             new_path = f"./saved_imgs/img_{i}.jpg"
             pil_image.save(new_path)
-            try:
-                encoding = find_face_encodings(new_path) 
-            except:
-                continue
-            i+= 1
-            face = Encoding(originalPath=image, originalImage=img, img=new_path, encoding=encoding)
-            faces.append(face)
-
+            
+            face = create_face_encoding(new_path, image)
+            if face is not None:
+                faces.append(face)
+                i+=1
+                
     return faces
 
-def main():
-    path = './img/20230903_135847190_iOS.jpg'
+
+def faces_from_input():
+    path = input("\nPlease provide the relative path to a photo that the reference individual is in\n >> ")
+    file_path = Path(path)
+
+    while not file_path.is_file():
+        path = input("Can't find file, please check input and try again (0 to exit) \n >> ")
+        if path == '0':
+            exit(0)
+        file_path = Path(path)
 
     ref_faces = find_all_faces_in_image(path)
 
-    ref_face = ref_faces[0]
-    compare_faces = find_faces_in_folder('./img')
+    if (len(ref_faces.keys()) == 0):
+        print("\nNo individuals found in the provided photo. Try again? Y/N")
+        while path.lower() != 'y' and path.lower() != 'n':
+            path = input("Try again? Y/N >>")
+        if path.lower() == 'y':
+            faces_from_input()
+        elif path.lower() == 'n':
+                exit(0)
+    
 
+    ref_image = input('\n./saved_imgs folder contains all the faces found in the photo. Please provide the path to desired individual (ex: ./saved_imgs/ref_img_0.jpg) \n >> ')
+    ref_image_path = Path(ref_image)
+
+    while not ref_image_path.is_file():
+        ref_image = input("Can't find file, please check input and try again (0 to exit) \n >> ")
+        if ref_image == '0':
+            exit(0)
+        ref_image_path = Path(ref_image)
+
+    face = create_face_encoding(ref_image, ref_faces.get(ref_image))
+    
+    if face is not None:
+        return face 
+    else: 
+        prompt = input("\nCould not find any faces in provided photo. Try again? Y/N>> ")
+        while prompt.lower() != 'y' and prompt.lower() != 'n':
+            prompt = input("Try again? Y/N >>")
+        if prompt.lower() == 'y':
+            faces_from_input()
+        else:
+            exit(0)
+
+def delete_saved_images(saved_images_path):
+    files_to_delete = glob.glob(f'{saved_images_path}/*')
+    for f in files_to_delete:
+        os.remove(f)
+
+def main():
+    saved_images_path = './saved_imgs'
+    stored_photos_path = './img'
+    delete_saved_images(saved_images_path)
+
+    if not os.path.exists(saved_images_path):
+        os.makedirs(saved_images_path)
+
+    ref_face  = faces_from_input()
+    compare_faces = find_faces_in_folder(stored_photos_path)
     result = find_all_matches(ref_face, compare_faces)
-
-    print(f"Reference is in these photos {result}")
+    
+    delete_saved_images(saved_images_path)
+        
+    print(f"Reference is in these photos: {result}")
 
     
 main()
